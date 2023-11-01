61d77dfed019b9195639e22a37f07de208de4e89
Lee Swindell <leeaswindell@gmail.com>
2023-11-01 15:32:58 -0400

    sizing and scrolling 2.0

--- scripts/dragscroll.js
+++ scripts/dragscroll.js
@@ -69,66 +69,67 @@ sliders.forEach((slider) => {
     momentumID: null
   });
 
+  let isDown = false;
+  let startX = 0;
+  let scrollLeft = 0;
+
   slider.addEventListener('mousedown', (e) => {
     isDown = true;
-    slider.classList.add('image-track-dragging')
+    slider.classList.add('image-track-dragging');
     startX = e.pageX - slider.offsetLeft;
     scrollLeft = slider.scrollLeft;
     cancelMomentumTracking(slider);
   });
 
-  slider.addEventListener('touchstart',(e) => {
+  slider.addEventListener('touchstart', (e) => {
     isDown = true;
-    slider.classList.add('image-track-dragging')
-    startX = e.pageX - slider.offsetLeft;
+    slider.classList.add('image-track-dragging');
+    startX = e.touches[0].pageX - slider.offsetLeft;
     scrollLeft = slider.scrollLeft;
     cancelMomentumTracking(slider);
-  })
-  
+  });
+
   slider.addEventListener('mouseleave', () => {
     isDown = false;
     slider.classList.remove('active');
   });
-  
-  
+
   slider.addEventListener('mouseup', () => {
     isDown = false;
     slider.classList.remove('image-track-dragging');
     beginMomentumTracking(slider);
   });
 
-  slider.addEventListener('touchend', (e) => {
+  slider.addEventListener('touchend', () => {
     isDown = false;
     slider.classList.remove('image-track-dragging');
     beginMomentumTracking(slider);
   });
-  
-  
+
   slider.addEventListener('mousemove', (e) => {
-    if(!isDown) return;
+    if (!isDown) return;
     e.preventDefault();
     const x = e.pageX - slider.offsetLeft;
-    const walk = (x - startX); //scroll-fast
-    var prevScrollLeft = slider.scrollLeft;
+    const walk = (x - startX);
+    const prevScrollLeft = slider.scrollLeft;
     slider.scrollLeft = scrollLeft - walk;
     momentumData.get(slider).velX = slider.scrollLeft - prevScrollLeft;
   });
 
   slider.addEventListener('touchmove', (e) => {
-    if(!isDown) return;
+    if (!isDown) return;
     e.preventDefault();
-    const x = e.pageX - slider.offsetLeft;
-    const walk = (x - startX) * 2; //scroll-fast
-    var prevScrollLeft = slider.scrollLeft;
+    const x = e.touches[0].pageX - slider.offsetLeft;
+    const walk = (x - startX);
+    const prevScrollLeft = slider.scrollLeft;
     slider.scrollLeft = scrollLeft - walk;
     momentumData.get(slider).velX = slider.scrollLeft - prevScrollLeft;
   });
 
   slider.addEventListener('wheel', (e) => {
     cancelMomentumTracking(slider);
-  });  
-})
-
+  });
+});
 
 function beginMomentumTracking(slider) {
   cancelMomentumTracking(slider);
