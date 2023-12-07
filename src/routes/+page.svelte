4d5bd5c96fdd32e85473824b8f0be93a33cdd8fc
Lee Swindell <leeaswindell@gmail.com>
2023-12-07 18:21:57 -0500

    mobile main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -143,4 +143,36 @@
 		margin-left: auto;
 		margin-right: auto;
 	}
+	@media (max-width: 450px) {
+		.grid {
+			display: flex;
+			flex-direction: column;
+			height: 100%;
+			max-height: 100%;
+		}
+
+		.bottom {
+			flex-direction: column;
+			background: linear-gradient(
+				180deg,
+				rgba(255, 0, 0, 1) 0%,
+				rgba(255, 154, 0, 1) 10%,
+				rgba(208, 222, 33, 1) 20%,
+				rgba(79, 220, 74, 1) 30%,
+				rgba(63, 218, 216, 1) 40%,
+				rgba(47, 201, 226, 1) 50%,
+				rgba(28, 127, 238, 1) 60%,
+				rgba(95, 21, 242, 1) 70%,
+				rgba(186, 12, 248, 1) 80%,
+				rgba(251, 7, 217, 1) 90%,
+				rgba(255, 0, 0, 1) 100%
+			);
+		}
+
+		.img-container {
+			width: 60%;
+			margin-top: 5px;
+			margin-bottom: 5px;
+		}
+	}
 </style>
