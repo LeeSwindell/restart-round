f8a6ee3e3745b100d3654d43e0236cf5150c1dbb
Lee Swindell <39568393+LeeSwindell@users.noreply.github.com>
2023-12-28 12:48:54 -0500

    newsletter??

--- src/lib/components/molepark/Extras.svelte
+++ src/lib/components/molepark/Extras.svelte
@@ -1,63 +1,69 @@
 <script lang="ts">
-	let one =   '/imgs/mole/MolePark1.jpg';
-	let two =   '/imgs/mole/MolePark2.jpg';
+	let one = '/imgs/mole/MolePark1.jpg';
+	let two = '/imgs/mole/MolePark2.jpg';
 	let three = '/imgs/mole/MolePark3.PNG';
-	let four =  '/imgs/mole/MolePark3.jpeg';
-	let five =  '/imgs/mole/MolePark4.jpeg';
+	let four = '/imgs/mole/MolePark3.jpeg';
+	let five = '/imgs/mole/MolePark4.jpeg';
 </script>
 
 <div>
 	<div class="container">
 		<div class="title">
-			<h1>Where did "Mole Park" come from?</h1>
+			<h1 class="title-text">Where did "Mole Park" come from?</h1>
 			<p class="mole-text">
 				Mole Park has always had a ton of dice.
-				<br><br>
-				We started <i>Mole Park: Whack & Win</i> at the end of a roadtrip through the Pacific northwest.
-				We talked of a new game, one that could be fun for everyone. A game - like a carnival - that was colorful and bright
-				and in your face from the start. Where you could get into the action before you even knew what you were doing,
-				and by the time you did figure everything out, you already had a winning streak going.
-				<br><br>
-				The original concept involved managing a squad of 5 moles. Each player started the game with a set of basic moles ("Budget Moles") and would use
-				their turns to whack stronger moles and replace their weakest links.
-				Over time, you would build up synergies between your moles and eventually you would have enough Power to swing a giant hammer and slam your way to the Grand Prize.
-			</p>		
+				<br /><br />
+				We started <i>Mole Park: Whack & Win</i> at the end of a roadtrip through the Pacific
+				northwest. We talked of a new game, one that could be fun for everyone. A game - like a
+				carnival - that was colorful and bright and in your face from the start. Where you could get
+				into the action before you even knew what you were doing, and by the time you did figure
+				everything out, you already had a winning streak going.
+				<br /><br />
+				The original concept involved managing a squad of 5 moles. Each player started the game with
+				a set of basic moles ("Budget Moles") and would use their turns to whack stronger moles and replace
+				their weakest links. Over time, you would build up synergies between your moles and eventually
+				you would have enough Power to swing a giant hammer and slam your way to the Grand Prize.
+			</p>
 		</div>
 		<div class="text-1">
 			<h2>Version 1</h2>
 			<p class="mole-text">
-				After I got home from the trip, I got right to work on a prototype and brought it on a family vacation. Here's a photo from our first game!
+				After I got home from the trip, I got right to work on a prototype and brought it on a
+				family vacation. Here's a photo from our first game!
 			</p>
 			<img src={one} class="person-img" alt="First Version" />
 		</div>
 		<div class="text-2">
 			<h2>Version 2</h2>
 			<p class="mole-text">
-				Once the circled numbers got too boring, I introduced a ton of new mechanics and printed out the cards:
+				Once the circled numbers got too boring, I introduced a ton of new mechanics and printed out
+				the cards:
 			</p>
-			<img src={two} class="person-img"  alt="Second Version" />
+			<img src={two} class="person-img" alt="Second Version" />
 		</div>
 		<div class="text-3">
 			<h2>Version 3</h2>
 			<p class="mole-text">
 				To play with Restart Round, I made an online version (via Tabletop Simulator):
 			</p>
-			<img src={three} class="person-img"  alt="Third Version" />
+			<img src={three} class="person-img" alt="Third Version" />
 		</div>
 		<div class="text-4">
 			<h2>Version 4</h2>
 			<p class="mole-text">
-				At this point, the game had become way too complicated. I scrapped everything and came up with the Whacking Dice as they are now.
-				This version of the game required over 130 dice per copy (shown here: the ones I didn't lose) since all of the prizes were just more dice:
+				At this point, the game had become way too complicated. I scrapped everything and came up
+				with the Whacking Dice as they are now. This version of the game required over 130 dice per
+				copy (shown here: the ones I didn't lose) since all of the prizes were just more dice:
 			</p>
-			<img src={four} class="person-img"  alt="Fourth Version" />
+			<img src={four} class="person-img" alt="Fourth Version" />
 		</div>
 		<div class="text-5">
 			<h2>Version 5</h2>
 			<p class="mole-text">
-				And then this turned into the game we all love! There were enough dice for 10 players, but you'd have to keep re-drawing the sharpie on every game, haha!
+				And then this turned into the game we all love! There were enough dice for 10 players, but
+				you'd have to keep re-drawing the sharpie on every game, haha!
 			</p>
-			<img src={five} class="person-img"  alt="Fifth Version" />
+			<img src={five} class="person-img" alt="Fifth Version" />
 		</div>
 	</div>
 </div>
@@ -68,21 +74,21 @@
 		margin: 20px;
 		border: 3px solid var(--green);
 		border-radius: 10px;
-		padding:20px;
+		padding: 20px;
 		background-color: tan;
 	}
 	.text-2 {
 		grid-area: text-2;
 		margin: 20px;
 		border-radius: 10px;
-		padding:20px;
+		padding: 20px;
 		border: 3px solid var(--green);
 		background-color: tan;
 	}
 	.text-3 {
 		grid-area: text-3;
 		border-radius: 10px;
-		padding:20px;	
+		padding: 20px;
 		margin: 20px;
 		border: 3px solid var(--green);
 		background-color: tan;
@@ -92,7 +98,7 @@
 		/* background-color: lightseagreen; */
 		border-radius: 10px;
 		margin: 20px;
-		padding:20px;
+		padding: 20px;
 		border: 3px solid var(--green);
 		background-color: tan;
 	}
@@ -101,7 +107,7 @@
 		/* background-color: lightseagreen; */
 		border-radius: 10px;
 		margin: 20px;
-		padding:20px;
+		padding: 20px;
 		border: 3px solid var(--green);
 		background-color: tan;
 	}
@@ -110,7 +116,7 @@
 		padding-top: 2.5vh;
 		padding-bottom: 2.5vh;
 		display: grid;
-		height:auto;
+		height: auto;
 		grid-template-areas:
 			'title title title title title'
 			'text-1 text-2 text-3 text-4 text-5';
@@ -128,12 +134,25 @@
 		--lighter-green: #bbf7d0;
 	}
 
-	.title {
+	/* .title {
 		grid-area: title;
 		font-family: 'Londrina Solid', sans-serif;
 		font-size: 3rem;
 		color: #38bdf8;
 		padding-bottom:6vh;
+	} */
+
+	.title {
+		grid-area: title;
+		/* font-family: 'Londrina Solid', sans-serif;
+		font-size: 2.5rem;
+		color: #38bdf8; */
+	}
+
+	.title-text {
+		font-family: 'Londrina Solid', sans-serif;
+		font-size: 2.5rem;
+		color: #38bdf8;
 	}
 
 	.mole-text {
--- src/routes/newsletter/+page.svelte
+++ src/routes/newsletter/+page.svelte
@@ -2,4 +2,66 @@
 	<title>Restart Round | Newsletter</title>
 </svelte:head>
 
-<div>newsletter coming soon!</div>
+<div class="container">
+	<div class="left">
+		<h1 class="title">What To Expect?</h1>
+		<p class="text">
+			Updates on games of course! We won't spam you, sell your email, or do anything but send you
+			the occasional email with news about our games.
+		</p>
+		<br />
+		<p class="text">Here's what we plan on sending:</p>
+		<ul>
+			<li class="text">Notifications that a Kickstarter went live</li>
+			<li class="text">A heads up that a game is released and ready to ship</li>
+			<li class="text">Plans for in-person events (e.g. Board game conventions)</li>
+		</ul>
+	</div>
+	<iframe
+		width="540"
+		height="305"
+		title="Newsletter Subscription"
+		src="https://ced175fe.sibforms.com/serve/MUIFACgU008ooNJdvSofuZVZbefrnfeHTV0RAd4mkJ-V12sn7Api-OBzNolSXOBm2p7FBxQSBZrQolRmE-k_C8iloaol_m0kmWMByTf7IVCd63hoTxxl1NZpQeBdlvNH2j_jtL7Jw0DTw0CLObiXqKgnitVAjrjray9-nf9xQaHjrRvNLiZO1lNPggI7Ysc1qpFl8f_05sRDiJ9i"
+		frameborder="0"
+		scrolling="auto"
+		allowfullscreen
+		style="display: block;max-width: 100%; overflow: visible;"
+	></iframe>
+</div>
+
+<style>
+	.container {
+		height: 85vh;
+		width: 90%;
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		margin: auto;
+		gap: 10px;
+		overflow: visible;
+	}
+
+	.left {
+		padding-top: 35px;
+		width: 540px;
+	}
+
+	.title {
+		grid-area: title;
+		font-family: 'Londrina Solid', sans-serif;
+		font-size: 3rem;
+		color: #38bdf8;
+	}
+
+	.text {
+		font-family: 'Londrina Solid', sans-serif;
+		font-weight: 300;
+		font-size: 1.5rem;
+		color: black;
+	}
+
+	iframe {
+		height: 100%;
+		overflow: visible;
+	}
+</style>
