b9090d39fff3818d55e62ea4bbfde2f1e7284157
Brian Ritchken <britchken@gmail.com>
2023-12-29 01:39:59 -0800

    Add text

--- src/lib/components/molepark/Extras.svelte
+++ src/lib/components/molepark/Extras.svelte
@@ -14,15 +14,23 @@
 				Mole Park has always had a ton of dice.
 				<br /><br />
 				We started <i>Mole Park: Whack & Win</i> at the end of a roadtrip through the Pacific
-				northwest. We talked of a new game, one that could be fun for everyone. A game - like a
+				northwest. Amid the trails and beaches, we talked of a new game, one that would be fun for everyone. A game - like a
 				carnival - that was colorful and bright and in your face from the start. Where you could get
 				into the action before you even knew what you were doing, and by the time you did figure
-				everything out, you already had a winning streak going.
+				everything out, you already had a winning streak going. But not one without strategy!
 				<br /><br />
-				The original concept involved managing a squad of 5 moles. Each player started the game with
-				a set of basic moles ("Budget Moles") and would use their turns to whack stronger moles and replace
-				their weakest links. Over time, you would build up synergies between your moles and eventually
-				you would have enough Power to swing a giant hammer and slam your way to the Grand Prize.
+				In the original concept of the game, you managed a squad of 5 moles which improved over time. You started the game with
+				a set of five "Budget Moles" (each worth one Power) and on your turn, you would spend your Power either to try to Whack better moles
+				to replace the weakest links in your squad, or to Win prizes (more dice). You could allocate your moles however you wanted, say by putting forth a few moles
+				to Whack and the rest to Win a prize. Over time, you would build up synergies between your moles, since most of them had either permanent abilities
+				or effects that triggered when they were recruited, and eventually you could Win the game by securing the Grand Prize with 50 Power.
+				<br /><br />
+				Aside from moles having effects and sticking with you (instead of being cashed in), one other notable change from the current version was that you
+				needed to hit each prize tier exactly to win a prize. If you wanted a Prize worth 30 power and you ended up with 32, you ended up with nothing.
+			   So at times, you might choose to keep a weaker mole over a stronger one simply because it gave you a total which could guarantee you a prize on your next turn.
+				If you didn't have an exact multiple of 5 with any subset of your moles (or if all the prizes at a certain tier were taken), you had to rely on rolling your prize dice
+				and hoping they would bring you to an exact multiple. Each dice you rolled could be rolled either as normal or as a “weak swing”, in which you divided the number you rolled by two.
+				Let's look at some photos!
 			</p>
 		</div>
 		<div class="text-1">
@@ -36,7 +44,7 @@
 		<div class="text-2">
 			<h2>Version 2</h2>
 			<p class="mole-text">
-				Once the circled numbers got too boring, I introduced a ton of new mechanics and printed out
+				At this time, all of the moles were simply numbers. Once that gameplay became stale, I introduced a ton of new mechanics and printed out
 				the cards:
 			</p>
 			<img src={two} class="person-img" alt="Second Version" />
@@ -44,7 +52,7 @@
 		<div class="text-3">
 			<h2>Version 3</h2>
 			<p class="mole-text">
-				To play with Restart Round, I made an online version (via Tabletop Simulator):
+				To play with the rest of Restart Round (who lived across the country), I made an online version (via Tabletop Simulator):
 			</p>
 			<img src={three} class="person-img" alt="Third Version" />
 		</div>
@@ -60,8 +68,8 @@
 		<div class="text-5">
 			<h2>Version 5</h2>
 			<p class="mole-text">
-				And then this turned into the game we all love! There were enough dice for 10 players, but
-				you'd have to keep re-drawing the sharpie on every game, haha!
+				And then this turned into the game we all love! There were enough dice for 10 players, if you didn't mind
+				re-drawing the dots on the dice with a sharpie whenever they rubbed off, haha!
 			</p>
 			<img src={five} class="person-img" alt="Fifth Version" />
 		</div>
