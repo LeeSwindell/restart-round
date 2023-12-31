e832c3dbad37b95eaf0b042a07a1abc4fdbeedc5
Brian Ritchken <britchken@gmail.com>
2023-12-31 15:12:43 -0800

    Add Billy text

--- src/routes/rosterreshuffle/+page.svelte
+++ src/routes/rosterreshuffle/+page.svelte
@@ -8,4 +8,39 @@
 
 <img style="display: block; margin-left:auto;margin-right:auto; width:50%; max-width:1000px; text-align:center;" src={logo} alt="Roster Reshuffle Logo"/>
 
-<div style="text-align: center; font-size: 60px">Coming soon!!!</div>
+<div style="margin:5%">
+	<p style="font-size:2.5vmin">
+		Restart Round brings you Roster Reshuffle, a football themed deck building card game currently in development.
+		<br>
+		The themed deck-builder is an extremely popular format but sports-based versions are suprisingly uncommon.
+		With Roster Reshuffle, we are aiming to create a game that combines the best of classic deck-builders and sports
+		card games into one.
+		<br><br>
+		At its core, the mechanics of Roster Reshuffle are fairly simple.  You start with basic players and use your turns
+		to upgrade them to more advanced and unique players.  Once you have an elite squad, you start scoring field goals
+		and touchdowns to try to beat out your opponent before the game ends.
+		<br><br>
+		What makes Roster Reshuffle unique as a deck-builder?  For one, you are actually building not only one deck, but two
+		decks: a deck of offensive players that gain yards and a deck of defensive players with abilities to stop the offense,
+		force turnovers, and wreak havoc.  The two deck format means that both players are engaged even when it isn't their turn.
+		Balancing these two decks is the biggest challenge of making Roster Reshuffle.  We still have more play-testing and tweaks
+		to go here, but are moving in the right direction with each successive round of play-testing.
+		<br><br>
+		The second unique feature of Roster Reshuffle: while you are building your offense and defense decks, they never go above
+		or below 11 cards.  As we have been play-testing Roster Reshuffle, the rule that really tied the game together is "get a
+		player, cut a player".  Like how football teams have a roster limit, your decks in Roster Reshuffle aren't endless.
+		Every time you buy a new player, you need to cut a player from either your discard pile or the top card of your deck.
+		This means you need to think twice about what you add and remove from your deck.  It also means you cycle through your
+		starter cards quickly to get to your new, powerful cards.  This was a huge pace of play improvement from early versions
+		of Roster Reshuffle.
+		<br><br>
+		The last major feature of Roster Reshuffle:  the running clock.  It's not uncommon for deck-builders to end when the deck
+		runs out.  This is no different in Roster Reshuffle.  However, the major difference in our game is that every turn, the
+		top card of the deck is destroyed, leading to the game getting increasingly closer to ending. With only 53 total cards
+		in the deck,  this means you have to take the lead and fast before the clock runs out!
+		<br><br>
+		The result of all of this is a fast-paced deckbuilding game that feels true to a real game of football.  When we playtested
+		the game and it ended 28-24 on a game-ending interception touchdown, we knew we had a good one!  More to come in 2024.
+		<br><br>
+	</p>
+</div>
