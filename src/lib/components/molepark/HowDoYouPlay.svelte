b644a2fdbbaee698d7359d6fc286ced0a6fc2b25
Brian Ritchken <britchken@gmail.com>
2024-01-12 00:14:40 -0800

    Update HowDoYouPlay.svelte

--- src/lib/components/molepark/HowDoYouPlay.svelte
+++ src/lib/components/molepark/HowDoYouPlay.svelte
@@ -105,7 +105,22 @@
 		margin: auto;
 	}
 
+	@media screen and (max-width: 450px) {
+		.container {
+			height: fit-content;
 
+			display: flex;
+			flex-direction: column;
+			row-gap: 10px;
+		}
+
+		.text-2 {
+			border: 3px solid var(--green);
+		}
+		.text-3 {
+			border: var(--darker-fuchsia) 3px solid;
+		}
+	}
 
 	.photo-container {
 		display: flex;
