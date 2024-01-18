35b5e468f28f695af0911bc19352da2f39d2ee31
Brian Ritchken <britchken@gmail.com>
2024-01-17 23:17:14 -0800

    Clean up video

--- src/lib/components/molepark/MoleParkLanding.svelte
+++ src/lib/components/molepark/MoleParkLanding.svelte
@@ -6,6 +6,13 @@
 		src="imgs/mole/Mole Park Logo.webp"
 		alt="King Mole"
 	/>
+	<h2 style="font-size:4vw;margin-left: auto; margin-right: auto; color: #8562AA; -webkit-text-stroke: 1px #000000; font-weight: extra-bold;">Mole Park Theme Song</h2>
+	<p><br /></p>
+	<div class="player">
+		<iframe width=480px height=270px src="https://www.youtube-nocookie.com/embed/lwtBjOKS1y0?si=YTL-oAxNUCuh22MH" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+	</div>
+	<p><br /></p>
+	<p><br /></p>
 </div>
 
 <style>
@@ -23,11 +30,22 @@
 		overflow-x: visible;
 		width: auto;
 		text-align: center;
-		height: 85vh;
-		max-height: 85vh;
 		display: flex;
 		flex-direction: column;
 		justify-content: center;
+	}
+
+	.player {
+ 	    border-radius: 20px;
+		border:white;
+		overflow: hidden;
+		z-index: 22;
+		height: 270px;
+		width: 480px;
+		margin-right: auto;
+		margin-left: auto;
+		border-style: ridge;
+	  	border: 5px solid #000; /* Border style, can be adjusted as needed */
 	}
 
 	@media screen and (max-width: 450px) {
