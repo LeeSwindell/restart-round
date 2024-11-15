0703e6cdf2faf0a629defa9716d344ce6e716499
Lee Swindell <leeaswindell@gmail.com>
2024-11-14 19:07:09 -0600

    fix sellfy scrolling

--- src/routes/store/+page.svelte
+++ src/routes/store/+page.svelte
@@ -3,13 +3,15 @@
 
 	onMount(() => {
 		let iframe = document.getElementById('sellfy-iframe');
+		let cont = document.getElementById('sellfy-container');
+
 		const observer = new ResizeObserver((entries) => {
 			for (let entry of entries) {
-				console.log('Element size changed to', entry.contentRect);
-				let ht = entry.contentRect.height;
-				let cont = document.getElementById('sellfy-container');
-				if (cont && iframe) {
-					cont.style.height = ht + 'px';
+				let newHeight = entry.contentRect.height;
+
+				// Avoid endless resizing by setting a maximum height limit
+				if (cont && newHeight < window.innerHeight) {
+					cont.style.height = `${newHeight}px`;
 				}
 			}
 		});
@@ -23,45 +25,32 @@
 <svelte:head>
 	<title>Restart Round | Store</title>
 </svelte:head>
-<main>
-	<div id="sellfy-container">
-		<iframe
-			id="sellfy-iframe"
-			title="store"
-			height="365"
-			width="100%"
-			src="https://restart-round.sellfy.store/embed/store/"
-			allowtransparency="true"
-		></iframe>
-		<script src="https://sellfy.com/js/api_buttons.js"></script>
-	</div>
-</main>
+
+<div id="sellfy-container">
+	<iframe
+		id="sellfy-iframe"
+		title="store"
+		width="100%"
+		src="https://restart-round.sellfy.store/embed/store/"
+		allowtransparency="true"
+	></iframe>
+	<script src="https://sellfy.com/js/api_buttons.js"></script>
+</div>
 
 <style>
-	main {
-		height: 100%;
-	}
-
 	#sellfy-container {
 		width: 80vw;
 		background-color: #fafafa;
-		height: 200vh;
 		margin-top: 20px;
 		margin-left: auto;
 		margin-right: auto;
-		overflow: hidden;
+		overflow: hidden; /* Prevent scrolling */
+		max-height: 90vh; /* Limit height expansion to 90% of the viewport */
 	}
 
-	/* add height changes for laptop/mobile to remove scrollbars? */
-	iframe {
+	#sellfy-iframe {
 		display: block;
-		padding: 0 !important;
-		height: 1200px;
 		width: 100%;
 		border: none;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: -10px;
-		overflow: hidden !important;
 	}
 </style>
